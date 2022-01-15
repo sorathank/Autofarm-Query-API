badb5939dcbc03f4e68329d0d514a5391df950c6
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2022-01-16 02:48:15 +0700

    Connect to BSC Network

--- src/autofarm/autofarm.service.ts
+++ src/autofarm/autofarm.service.ts
@@ -1,25 +1,41 @@
+import { JsonRpcProvider } from '@ethersproject/providers';
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
+import { Contract, ethers } from 'ethers';
+import * as MASTERCHEF_ABI from '../abi/masterchef.abi.json';
+import * as ERC20_ABI from '../abi/erc20.abi.json'
+import * as CAKELP_ABI from '../abi/cakeLP.abi.json'
+import { getJsonRPCURL, getMasterChefContract } from 'src/config/rpc.config';
 
 @Injectable()
 export class AutofarmService {
+    private provider: JsonRpcProvider;
+    private masterchef: Contract
+
     constructor(
         @Inject(CACHE_MANAGER)
-        private cacheManager: Cache
-    ) {}
+        private cacheManager: Cache,
+    ) {
+        this.provider = new ethers.providers.JsonRpcProvider(getJsonRPCURL())
+        this.masterchef = new Contract(getMasterChefContract(), MASTERCHEF_ABI, this.provider);
+    }
 
     async updateCache(): Promise<string>{
-        const cache = await this.cacheManager.get("cache")
-        console.log(cache)
-        if (cache){
-            return typeof(cache)
-        } 
-        return "Cache Unavailable"
+        const cachedPoolLength = await this.cacheManager.get("poolLength");
+        const latestPoolLength = Number((await this.masterchef.poolLength())._hex)
+        if (!cachedPoolLength || latestPoolLength) {
+            this.cacheManager.set("poolLength", latestPoolLength);
+            console.log(await this.cacheManager.get('poolLength'))
+        }
+        
+        return "Cache Updated"
     }
 
     async getAddressInformation(address: string): Promise<string>{
         await this.cacheManager.set("cache", "Updated")
         console.log(await this.cacheManager.get("cache"))
         return await this.cacheManager.get("cache")
+        return "Hello"
     }
+
 }
