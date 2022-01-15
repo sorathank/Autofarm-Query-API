208846dbb77a96832fd30a235dde6492220c4f41
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2022-01-16 03:29:08 +0700

    Use Multicall for improving performance

--- src/autofarm/autofarm.service.ts
+++ src/autofarm/autofarm.service.ts
@@ -1,31 +1,44 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
-import { Contract, ethers } from 'ethers';
+import { ethers } from 'ethers';
 import * as MASTERCHEF_ABI from '../abi/masterchef.abi.json';
 import * as ERC20_ABI from '../abi/erc20.abi.json'
 import * as CAKELP_ABI from '../abi/cakeLP.abi.json'
 import { getJsonRPCURL, getMasterChefContract } from 'src/config/rpc.config';
+import { Contract, Provider } from 'ethers-multicall';
 
 @Injectable()
 export class AutofarmService {
     private provider: JsonRpcProvider;
-    private masterchef: Contract
+    private masterchef: ethers.Contract;
+    private multicall;
+    private multicallMasterChef;
 
     constructor(
         @Inject(CACHE_MANAGER)
         private cacheManager: Cache,
     ) {
         this.provider = new ethers.providers.JsonRpcProvider(getJsonRPCURL())
-        this.masterchef = new Contract(getMasterChefContract(), MASTERCHEF_ABI, this.provider);
+        this.multicall = new Provider(this.provider, 56);
+        this.masterchef = new ethers.Contract(getMasterChefContract(), MASTERCHEF_ABI, this.provider);
+        this.multicallMasterChef = new Contract(getMasterChefContract(), MASTERCHEF_ABI)
     }
 
     async updateCache(): Promise<string>{
         const cachedPoolLength = await this.cacheManager.get("poolLength");
         const latestPoolLength = Number((await this.masterchef.poolLength())._hex)
+
         if (!cachedPoolLength || latestPoolLength) {
             this.cacheManager.set("poolLength", latestPoolLength);
-            console.log(await this.cacheManager.get('poolLength'))
+            let temp = [];
+            for (var i = 0; i<latestPoolLength; i++) {
+                temp.push(this.multicallMasterChef.poolInfo(i));
+            }
+            const res = await this.multicall.all(temp);
+            res.forEach(x => {
+                console.log(x);
+            });
         }
         
         return "Cache Updated"
