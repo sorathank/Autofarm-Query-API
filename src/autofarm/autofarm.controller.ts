22ef7d3e7fbbd59d42d628ca75d30e064ebc8319
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2022-01-15 15:15:53 +0700

    init endpoints

--- src/autofarm/autofarm.controller.ts
+++ src/autofarm/autofarm.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AutofarmService } from './autofarm.service';
 
 @Controller('autofarm')
-export class AutofarmController {}
+export class AutofarmController {
+    constructor(private readonly autofarmService: AutofarmService){}
+
+    @Get('/cache/update')
+    async updateCache(): Promise<string>{
+        return "Cache Updated"
+    }
+
+    @Get(':address')
+    async getStakedBalance(
+        @Param('address')
+        address: string
+    ): Promise<string>{
+        return address
+    }
+}
