9fd338a194ad0169a043182b725f48de858c12fd
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2022-01-17 21:39:51 +0700

    Move farm interface into dto

---
+++ src/autofarm/dto/farm.dto.ts
@@ -0,0 +1,12 @@
+export interface TokenDto {
+    symbol: string,
+    address: string,
+    balance: number
+}
+
+export interface FarmDto {
+    tokens: TokenDto[],
+    balance: number,
+    lpAddress?: string,
+    rewards: TokenDto[],
+}
