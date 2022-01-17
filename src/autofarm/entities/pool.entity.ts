e1fdc112e226452fe3e64f2841004a22b069bad1
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2022-01-17 18:00:27 +0700

    Add Pool Interface

---
+++ src/autofarm/entities/pool.entity.ts
@@ -0,0 +1,28 @@
+export interface TempPool {
+    [key: string]: any
+}
+
+export interface Token {
+    tokenAddress: string,
+    tokenDecimal: number,
+    tokenSymbol: string
+}
+
+export enum TokenType {
+    LP = "lp",
+    Single = "single"
+}
+export interface Pool {
+    poolId: number,
+
+    // LP
+    lpAddress?: string,
+    lpDecimals?: number,
+
+    // Token
+    token?: Token,
+    token0?: Token,
+    token1?: Token,
+
+    type?: TokenType
+}
